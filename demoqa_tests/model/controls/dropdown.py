3deba8ad20b24362ef7a7c1b95a3593d0ea705da
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-10-14 02:53:13 +0300

    HW7

---
+++ demoqa_tests/model/controls/dropdown.py
@@ -0,0 +1,15 @@
+from selene import have
+from selene.support.shared import browser
+
+
+def select(element, option):
+    element.click()
+    browser.all('[id^=react-select][id*=-option-]').by(
+        have.exact_text(option)
+    ).click()
+
+
+def select_by_typing(element, option):
+    element.type(option).press_enter()
+
+
