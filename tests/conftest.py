c1faa78a9b7af509858ac0e64844afda022a46d3
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-10-23 20:09:50 +0300

    conftest.py tinkering

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,36 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-
+from dotenv import load_dotenv
 
 from demoqa_tests.utils import attach
 
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
 
 @pytest.fixture(scope='function')
-def setup_browser():
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -20,8 +38,11 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser = Browser(Config(driver))
@@ -33,23 +54,3 @@ def setup_browser():
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
-
-
-
-
-
-#import os
-#
-#from selene.support.shared import browser
-#import pytest
-#
-#
-#@pytest.fixture(scope='function', autouse=True)
-#def browser_management():
-#    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
-#    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
-#    browser.config.hold_browser_open = (
-#        os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
-#    )
-#    browser.config.timeout = float(os.getenv('selene.timeout', '3'))
-#
